6318579e0d86fc89b79dbf2cd04da3931d4bf758
shivanya-chandra <69587802+shivanya-chandra@users.noreply.github.com>
2024-11-18 20:26:31 -0500

    a few more fixes

--- Fall_2024/shivanya/SvsP_within/experiment.js
+++ Fall_2024/shivanya/SvsP_within/experiment.js
@@ -49,9 +49,74 @@ class Experiment {
       response_count: 0,
       data: { phase: "instructions" },
     };
-
+    //this is with Vb stimuli 
     this.timeline.push(instructionTrial);
-    this.practiceList = jsPsych.randomization.shuffleNoRepeats(this.stimuli["Practice"]);
+    this.practiceList = jsPsych.randomization.shuffleNoRepeats(this.stimuli["Vb"]);
+
+    for (let ex of this.practiceList) {
+      let task = {
+        type: "pcllab-core",
+        stimuli: [
+          { text: `<div style="text-align: center; font-size: 24px">${ex.cue}</div>` }
+        ],
+        response_type: "input",
+        response_count: 1,
+        data: { phase: "practiceTest", cue: ex.cue }
+      };
+
+      let anotherTask = {
+        type: "pcllab-core",
+        stimuli: [
+          { text: `<div style="text-align: center; font-size: 24px">${ex.cue} -- ${ex.target}</div>` }
+        ],
+        response_count: 0,
+        maximum_time: 5000,
+        data: { phase: "practicePair", cue: ex.cue, target: ex.target },
+      }
+      this.timeline.push(task);
+      this.timeline.push(anotherTask);
+    }
+    // Math Distractor (Addition problems)
+    const mathStimuli = [];
+    const correctAnswers = [];
+    for (let i = 0; i < 15; i++) {
+      const num1 = Math.floor(Math.random() * 9 + 1);
+      const num2 = Math.floor(Math.random() * 9 + 1);
+      const num3 = Math.floor(Math.random() * 9 + 1);
+
+      correctAnswers.push(num1 + num2 + num3);
+      const equation = `${num1} + ${num2} + ${num3}`;
+
+      mathStimuli.push({
+        text: `<div style="text-align: center; font-size: 24px">${equation}</div>`,
+      });
+    }
+
+    const mathDistractor = {
+      type: "pcllab-core",
+      stimuli: mathStimuli,
+      total_maximum_time: 1000 * 120,
+      response_type: "input",
+      response_count: 1,
+      show_button: true,
+      button_text: "Next",
+      data: { phase: "distractor", correctAnswers: correctAnswers },
+    };
+    this.timeline.push(mathDistractor);
+
+    // Final Test
+    for (let ex of this.practiceList) {
+      let cuedRecallTask = {
+        type: "pcllab-core",
+        stimuli: [{ text: `<div style="text-align: center; font-size: 24px">${ex.cue}</div>` }],
+        response_type: "input",
+        response_count: 1,
+        maximum_time: 7000,
+        data: { phase: "finalTest", cue: ex.cue },
+      };
+      this.timeline.push(cuedRecallTask);
+    }
+
   }
 
 
